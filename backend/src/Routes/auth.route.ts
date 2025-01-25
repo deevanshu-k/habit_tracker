9be77577dd357766b3722ed0fd042859d03effa5
Deevanshu Kushwah <astroboiscosmos@gmail.com>
2025-01-26 01:52:38 +0530

    POST /v1/api/auth/local/signup/verify Created

--- backend/src/Routes/auth.route.ts
+++ backend/src/Routes/auth.route.ts
@@ -1,9 +1,21 @@
 import { Router } from "express";
-import { localSignUp } from "../controllers/auth.controller";
+import {
+    localSignUp,
+    localSignUpVerifyByOTP,
+} from "../controllers/auth.controller";
 const router = Router();
 
 router.post("/local/signup", localSignUp.validator, localSignUp.controller);
-router.post("/local/signin", (req, res) => {});
+router.post(
+    "/local/signup/verify",
+    localSignUpVerifyByOTP.validator,
+    localSignUpVerifyByOTP.controller
+);
+router.post(
+    "/local/signin",
+    localSignUpVerifyByOTP.validator,
+    localSignUpVerifyByOTP.controller
+);
 router.post("/local/forgot_pswd", (req, res) => {});
 router.post("/local/forgot_pswd/check_otp", (req, res) => {});
 
--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -4,6 +4,7 @@ import db from "../services/db.service";
 import { generateOTP } from "../services/otp.service";
 import { otpSignUpCacheManager } from "../services/cache.service";
 import { sendMail } from "../services/mail.service";
+import * as bcrypt from "bcrypt";
 
 export const localSignUp = {
     validator: celebrate({
@@ -65,6 +66,85 @@ export const localSignUp = {
             });
         } catch (error) {
             res.json({
+                code: 500,
+                message: "Something went wrong!",
+            });
+        }
+    },
+};
+
+export const localSignUpVerifyByOTP = {
+    validator: celebrate({
+        body: Joi.object({
+            email: Joi.string().email().required(),
+            otp: Joi.string().length(6).pattern(/^\d+$/).required(),
+        }),
+    }),
+    controller: async (req: Request, res: Response) => {
+        try {
+            const { email, otp } = req.body;
+
+            // Check if user already exist
+            let user = await db.user.findFirst({
+                where: {
+                    email: email,
+                },
+            });
+            if (user) {
+                res.status(409).json({
+                    code: 409,
+                    message: "User with this email already exist!",
+                });
+                return;
+            }
+
+            // Check otp expiration
+            const cacheEntry = otpSignUpCacheManager.get(email);
+            if (!cacheEntry) {
+                res.status(409).json({
+                    code: 500,
+                    message: "OTP expired!",
+                });
+                return;
+            }
+
+            // Compare otp
+            if (cacheEntry.otp !== otp) {
+                res.status(409).json({
+                    code: 409,
+                    message: "Wrong OTP!",
+                });
+                return;
+            }
+
+            // Create password hash with hash salt
+            const salt_rounds = 10;
+            const salt = await bcrypt.genSalt(salt_rounds);
+            const hashed_password = await bcrypt.hash(
+                cacheEntry.password,
+                salt
+            );
+
+            // Save user
+            await db.user.create({
+                data: {
+                    email,
+                    first_name: cacheEntry.first_name,
+                    last_name: cacheEntry.last_name,
+                    pswd_hash: hashed_password,
+                    pswd_hash_salt: salt,
+                },
+            });
+
+            // Remove user signup cache
+            otpSignUpCacheManager.delete(email);
+
+            res.status(200).json({
+                code: 200,
+                message: "Welcome to habit tracker!",
+            });
+        } catch (error) {
+            res.status(500).json({
                 code: 500,
                 message: "Something went wrong!",
             });
