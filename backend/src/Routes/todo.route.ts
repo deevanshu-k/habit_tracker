66b040249e7da65a8b802933f31d92cfa7f3596e
Deevanshu Kushwah <astroboiscosmos@gmail.com>
2025-02-09 00:32:34 +0530

    DELETE /v1/api/todo/:id Delete todo

--- backend/src/Routes/todo.route.ts
+++ backend/src/Routes/todo.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
     createTodo,
+    deleteTodo,
     getTodayTodos,
     updateTodo,
 } from "../controllers/todo.controller";
@@ -10,6 +11,6 @@ router.get("/", (req, res) => {});
 router.get("/today", getTodayTodos.validator, getTodayTodos.controller);
 router.post("/", createTodo.validator, createTodo.controller);
 router.put("/:id", updateTodo.validator, updateTodo.controller);
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", deleteTodo.validator, deleteTodo.controller);
 
 export default router;
--- backend/src/controllers/todo.controller.ts
+++ backend/src/controllers/todo.controller.ts
@@ -161,3 +161,45 @@ export const updateTodo = {
         }
     },
 };
+
+export const deleteTodo = {
+    validator: celebrate({
+        params: Joi.object({
+            id: Joi.string().required(),
+        }),
+    }),
+    controller: async (req: AuthenticatedRequest, res: Response) => {
+        try {
+            if (!req.user) {
+                res.json({
+                    code: 401,
+                    message: UNAUTHORIZED_REQUEST,
+                });
+                return;
+            }
+
+            // Delete todo
+            const { id } = req.params;
+            const todo = await db.todo.delete({
+                where: {
+                    userId: req.user.id,
+                    id: id,
+                },
+                select: {
+                    id: true,
+                },
+            });
+
+            res.status(200).json({
+                code: 200,
+                message: SUCCESS,
+                data: todo,
+            });
+        } catch (error) {
+            res.status(500).json({
+                code: 500,
+                message: SOMETHING_WENT_WRONG,
+            });
+        }
+    },
+};
