d6b298a856173f77e621e94dfae350caad8c4855
Deevanshu Kushwah <astroboiscosmos@gmail.com>
2025-02-08 21:04:53 +0530

    Update habitLog in todays todo page functionality added

--- frontend/src/pages/todo/todo-body/todo-body-habitlist/todo-body-habitlist.tsx
+++ frontend/src/pages/todo/todo-body/todo-body-habitlist/todo-body-habitlist.tsx
@@ -1,20 +1,50 @@
 import { Pencil2Icon, PlusCircledIcon, TrashIcon } from "@radix-ui/react-icons";
 import { Box, Checkbox, Flex, Text } from "@radix-ui/themes";
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { StoreState, TodayHabit } from "../../../../store/store.type";
+import { Dispatch } from "redux";
+import {
+    updateHabitLogAction,
+    UpdateHabitLogAction,
+} from "../../../../store/habit/habit.action";
 
 const TodoBodyHabitlist: React.FC = ({}) => {
     const habits = useSelector<StoreState, TodayHabit[]>(
         (s) => s.habit.today.data
     );
+    const dispatch = useDispatch<Dispatch<UpdateHabitLogAction>>();
+    const now = new Date();
+    const updateHabitStatus = (
+        habit_id: string,
+        is_done: boolean,
+        note: string
+    ) => {
+        dispatch(
+            updateHabitLogAction(
+                habit_id,
+                now.getDate(),
+                now.getMonth() + 1,
+                now.getFullYear(),
+                is_done,
+                note
+            )
+        );
+    };
     return (
         <Box className="w-full">
             <Flex direction="column" gap="3">
                 {habits.map((h) => (
                     <Box className="px-4 rounded border border-[var(--gray-7)] w-full h-[50px] flex flex-row gap-4 justify-between items-center">
                         <div>
-                            <Checkbox checked={h.is_done} className="" size="3" />
+                            <Checkbox
+                                checked={h.is_done}
+                                onClick={() =>
+                                    updateHabitStatus(h.id, !h.is_done, "")
+                                }
+                                className="cursor-pointer"
+                                size="3"
+                            />
                         </div>
                         <div className="flex-grow">
                             <Text>{h.title}</Text>
