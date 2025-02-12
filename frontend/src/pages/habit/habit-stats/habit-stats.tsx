c2ca081828e135c78ba3f96dd6741a30d5be2776
Deevanshu Kushwah <astroboiscosmos@gmail.com>
2025-02-12 23:13:20 +0530

    Sidebar ui functionality added for displaying habits

--- frontend/src/pages/habit/habit-stats/habit-stats.tsx
+++ frontend/src/pages/habit/habit-stats/habit-stats.tsx
@@ -8,8 +8,8 @@ import {
     FetchHabitAction,
 } from "../../../store/habit/habit.action";
 import { Dispatch } from "redux";
-import { Box, Button, Flex, Text } from "@radix-ui/themes";
-import { PlusIcon } from "@radix-ui/react-icons";
+import { Flex } from "@radix-ui/themes";
+import HabitStickySideBar from "./habit-sticky-sidebar/habit-sticky-sidebar";
 
 const HabitStats: React.FC = () => {
     const now = new Date();
@@ -55,50 +55,7 @@ const HabitStats: React.FC = () => {
             </div>
             {/* Sticky Sidebar */}
             <div className="w-full lg:w-[400px] lg:self-start lg:sticky lg:top-10 p-4 border-2 border-[var(--gray-7)]">
-                <Text align="center" as="div" color="gray" className="p-4">
-                    Habits
-                </Text>
-                <Flex direction="column" gap={"4"}>
-                    <Box className="w-full relative group shadow-[var(--grass-2)] shadow-sm">
-                        <div className="flex flex-row justify-between p-3 shadow-lg">
-                            <Text as="div" color="gray">
-                                Go to gym
-                            </Text>
-                            <Text>8/10</Text>
-                        </div>
-                        <div className="absolute top-0 hidden group-hover:flex flex-row transition-all duration-300 justify-evenly items-center w-full h-full p-2 bg-[var(--gray-2)]">
-                            <Button
-                                className="w-[30%] cursor-pointer"
-                                variant="ghost"
-                                color="grass"
-                            >
-                                Edit
-                            </Button>
-                            <Button
-                                className="w-[30%] cursor-pointer"
-                                variant="ghost"
-                                color="grass"
-                            >
-                                Delete
-                            </Button>
-                            <Button
-                                className="w-[30%] cursor-pointer"
-                                variant="ghost"
-                                color="grass"
-                            >
-                                Archive
-                            </Button>
-                        </div>
-                    </Box>
-                    <Button
-                        variant="soft"
-                        color="grass"
-                        className="w-full h-[40px]"
-                    >
-                        <PlusIcon />
-                        <Text>Add Habit</Text>
-                    </Button>
-                </Flex>
+                <HabitStickySideBar />
             </div>
         </Flex>
     );
---
+++ frontend/src/pages/habit/habit-stats/habit-sticky-sidebar/habit-sticky-sidebar.tsx
@@ -0,0 +1,69 @@
+import { PlusIcon } from "@radix-ui/react-icons";
+import { Box, Button, Flex, Text } from "@radix-ui/themes";
+import React from "react";
+import { useSelector } from "react-redux";
+import { Habit, StoreState } from "../../../../store/store.type";
+
+const HabitStickySideBar: React.FC = ({}) => {
+    const habits = useSelector<StoreState, Habit[]>((s) => s.habit.data);
+    return (
+        <>
+            <Text align="center" as="div" color="gray" className="p-4">
+                Habits
+            </Text>
+            <Flex direction="column" gap={"4"}>
+                {habits.map((habit) => (
+                    <Box className="w-full relative group shadow-[var(--grass-2)] shadow-sm">
+                        <div className="flex flex-row justify-between p-3 shadow-lg">
+                            <Text as="div" color="gray">
+                                {habit.title}
+                            </Text>
+                            <Text>
+                                {habit.logs.reduce(
+                                    (p, h) => p + Number(h.is_done),
+                                    0
+                                ) +
+                                    "/" +
+                                    "10"}
+                            </Text>
+                        </div>
+                        <div className="absolute top-0 hidden group-hover:flex flex-row transition-all duration-300 justify-evenly items-center w-full h-full p-2 bg-[var(--gray-2)]">
+                            <Button
+                                className="w-[30%] cursor-pointer"
+                                variant="ghost"
+                                color="grass"
+                            >
+                                Edit
+                            </Button>
+                            <Button
+                                className="w-[30%] cursor-pointer"
+                                variant="ghost"
+                                color="grass"
+                            >
+                                Delete
+                            </Button>
+                            <Button
+                                className="w-[30%] cursor-pointer"
+                                variant="ghost"
+                                color="grass"
+                            >
+                                Archive
+                            </Button>
+                        </div>
+                    </Box>
+                ))}
+
+                <Button
+                    variant="soft"
+                    color="grass"
+                    className="w-full h-[40px]"
+                >
+                    <PlusIcon />
+                    <Text>Add Habit</Text>
+                </Button>
+            </Flex>
+        </>
+    );
+};
+
+export default HabitStickySideBar;
